b99bade5b866bc651e77e217acc78d5660e237d3
mair <mair.swartz@live.com>
2023-12-28 13:29:57 +1100

    finalize main

--- main/main.c
+++ main/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+// #include "esp_log.h"
 #include "cchalk.h"
 
 void app_main(void)
 {
+
+    // ESP_LOGI("main", "A log message");
+    // ESP_LOGI("main", "Another message I don't core about");
+    // ESP_LOGI("main", "And yet another");
+    // chalk_printf_background_ln(CHALK_DARKBLUE, CHALK_CYAN, "finally something I do care about");
+    // ESP_LOGI("main", "Some other message I don't care about");
+
     chalk_printf(CHALK_YELLOW, "some yellow text\n");
     chalk_printf_ln(CHALK_RED, "some red text with a new line added");
     chalk_printf_ln(CHALK_GREEN, "this is green and is line number %d", 3);
@@ -11,10 +19,8 @@ void app_main(void)
 
     // chalk_printf_background(CHALK_BLACK, CHALK_WHITE, "this will bleed colour into the next line\n");
 
-    chalk_printf_foreground_rgb(0, 0, 125, "this is text using rgb values\n");
+    chalk_printf_foreground_rgb(0, 255, 255, "this is text using rgb values\n");
     chalk_rgb_ln(255, 100, 100, 50, 255, 50, "this is text and background using rgb values");
-
-    chalk_printf_ln(CHALK_RED, "some red text with a new line added");
 
     chalk_printf_ln(CHALK_BOLD, "text with CHALK_BOLD decoration");
     chalk_printf_ln(CHALK_UNDERLINE, "text with CHALK_UNDERLINE decoration");
